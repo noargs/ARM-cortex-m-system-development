4223c22e6f336e652848570c002f0c7a2328e32d
Ibn Asghar <asim.ibn.asghar@gmail.com>
2022-06-16 05:44:24 +0100

    init task | scheduler stack

---
+++ task_scheduler/Inc/main.h
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+#define MAX_TASKS			 4
+#define DUMMY_XPSR			 0x01000000U
+
+/* stacks memory calculation */
+#define SIZE_TASK_STACK      1024U
+#define SIZE_SCHED_STACK     1024U
+
+#define SRAM_START           0x20000000U
+#define SIZE_SRAM            ( (128) * (1024) )
+#define SRAM_END             ( (SRAM_START) + (SIZE_SRAM) )
+
+
+#define T1_STACK_START       (SRAM_END)
+#define T2_STACK_START       ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
+#define T3_STACK_START       ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
+#define T4_STACK_START       ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START    ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+
+
+#define TICK_HZ              1000U  // 1000Hz
+
+#define HSI_CLOCK            16000000U  // 1600000Hz OR 16Mhz
+#define SYSTICK_TIM_CLK      HSI_CLOCK
+
--- task_scheduler/Src/main.c
+++ task_scheduler/Src/main.c
@@ -1,41 +1,32 @@
-#include <stdint.h>
 #include <stdio.h>
+#include "main.h"
 
-//#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-//  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-//#endif
 
 void task1_handler(void); // task1
 void task2_handler(void); // task2
 void task3_handler(void); // task3
 void task4_handler(void); // task4 of the application
 
-/* stacks memory calculation */
-#define SIZE_TASK_STACK      1024U
-#define SIZE_SCHED_STACK     1024U
-
-#define SRAM_START           0x20000000U
-#define SIZE_SRAM            ( (128) * (1024) )
-#define SRAM_END             ( (SRAM_START) + (SIZE_SRAM) )
-
-
-#define T1_STACK_START       (SRAM_END)
-#define T2_STACK_START       ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
-#define T3_STACK_START       ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
-#define T4_STACK_START       ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
-#define SCHED_STACK_START    ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
-
-
-#define TICK_HZ              1000U  // 1000Hz
-
-#define HSI_CLOCK            16000000U  // 16000000Hz OR 16Mhz
-#define SYSTICK_TIM_CLK      HSI_CLOCK
-
 
 void init_systick_timer(uint32_t tick_hz);
+__attribute__((naked)) void init_scheduler_stack(uint32_t sched_stack_start);
+void init_task_stack();
+
+
+uint32_t psp_of_tasks[MAX_TASKS] = {T1_STACK_START, T2_STACK_START, T3_STACK_START, T4_STACK_START};
+uint32_t task_handlers[MAX_TASKS];
 
 int main(void)
 {
+	init_scheduler_stack(SCHED_STACK_START);
+
+
+	task_handlers[0] = (uint32_t)task1_handler;
+	task_handlers[1] = (uint32_t)task2_handler;
+	task_handlers[2] = (uint32_t)task3_handler;
+	task_handlers[3] = (uint32_t)task4_handler;
+
+	init_task_stack();
 
 	init_systick_timer(TICK_HZ);
 
@@ -76,19 +67,59 @@ void task4_handler(void)
 
 void init_systick_timer(uint32_t tick_hz)
 {
-	uint32_t *p_srvr = (uint32_t*) 0xE000E014;
+	uint32_t *p_scsr = (uint32_t*)0xE000E010;
+	uint32_t *p_srvr = (uint32_t*)0xE000E014;
+
+	// to generate exception every 100 clock cycles use 100 - 1 = 99 reload value for SRVR
 	uint32_t count_value = (SYSTICK_TIM_CLK / tick_hz) - 1;
 
 	// Clear the value of SVR
 	*p_srvr &= ~(0x00FFFFFF);
 
 	// load the value into SVR
-	*p_srvt |= count_value;
+	*p_srvr |= count_value;
 
 	// do some settings
-
+	*p_scsr |= (1 << 1); // Enable SysTick Exception Request
+	*p_scsr |= (1 << 2); // Indicates the clock source, processor clock source
 
 	//enable the systick
+	*p_scsr |= (1 << 0); // Enables the counter
+
+}
+
+__attribute__((naked)) void init_scheduler_stack(uint32_t sched_stack_start){
+	__asm volatile("MSR MSP,%0": : "r"(sched_stack_start) : );
+	__asm volatile("BX LR");
+}
+
+void init_task_stack(){
+
+	uint32_t *psp;
+	for(int i=0; i < MAX_TASKS; i++){
+		psp = (uint32_t*) psp_of_tasks[i];
+
+		psp--;
+		*psp = DUMMY_XPSR; // xPSR 0x00100000
+
+		psp--; // PC
+		*psp = task_handlers[i];
+
+		psp--; // LR - page[2-28] ARM Cortex-M4 DUI0553
+		*psp = 0xFFFFFFFD;
+
+		for(int j=0; j < 13; j++){
+			psp--;
+			*psp = 0;
+		}
+
+		psp_of_tasks[i] = (uint32_t)psp;
+
+	}
+}
+
+void SysTick_Handler(void)
+{
 
 }
 
